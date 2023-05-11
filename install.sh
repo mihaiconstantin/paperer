905af007433ce7716c970fb9e3d37357fcefb17a
mihaiconstantin <mihai@mihaiconstantin.com>
2023-05-12 00:59:50 +0200

    Refactor: update installation location variable

--- install.sh
+++ install.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-# User feedback.
-echo "\nPreparing the installation..."
-
 # Define the install directory.
 INSTALL_DIR=/usr/local
 
 # Define the executable directory.
 BIN_DIR=/usr/local/bin
 
+# Create the full path for the `paperer` installation.
+INSTALL_DIR=$INSTALL_DIR/paperer
+
 # User feedback.
-echo "Cloning the 'mihaiconstantin/paperer' repository..."
+echo " + Cloning the 'paperer' repository..."
 
 # Close the repository at the install location.
-git clone --depth 1 https://github.com/mihaiconstantin/paperer.git $INSTALL_DIR/paperer &> /dev/null
+git clone --depth 1 https://github.com/mihaiconstantin/paperer.git $INSTALL_DIR &> /dev/null
 
 # User feedback.
-echo "Installing 'paperer'..."
+echo " + Installing 'paperer'..."
 
 # Rename the executable.
-mv $INSTALL_DIR/paperer/paperer.sh $INSTALL_DIR/paperer/paperer
+cp $INSTALL_DIR/paperer.sh $INSTALL_DIR/paperer
 
 # Make the executable executable.
-chmod +x $INSTALL_DIR/paperer/paperer
+chmod +x $INSTALL_DIR/paperer
 
 # Create a symbolic link to the executable.
-ln -s $INSTALL_DIR/paperer/paperer $BIN_DIR/paperer
+ln -sf $INSTALL_DIR/paperer $BIN_DIR/paperer
 
 # User feedback.
-echo "Installed 'paperer' successfully! Type 'paperer --help' to get started."
+echo " + Installed 'paperer' successfully! Type 'paperer --help' to get started."
